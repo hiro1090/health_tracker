c4c60cb60c49a6db800da079e022634cd60b72f2
hiro1090 <king1356488@gmail.com>
2024-06-25 22:45:01 +0900

    体重記録の編集フォームとBMI計算結果の表示法の実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def calculate_bmi(weight, height)
+    bmi = (weight / (height.to_f / 100) ** 2).round(1)#BMIの計算式
+    bmi_status(bmi)
+  end
+
+  def bmi_status(bmi)
+    case bmi
+    when 0...18.5
+      "<span class='text-primary'>Underweight (#{bmi})</span>".html_safe
+    when 18.5...25
+      "<span class='text-success'>Normal (#{bmi})</span>".html_safe
+    when 25...30
+      "<span class='text-warning'>Overweight (#{bmi})</span>".html_safe
+    else
+      "<span class='text-danger'>Obese (#{bmi})</span>".html_safe
+    end
+  end
 end
---
+++ db/migrate/20240625132454_add_user_to_weight_records.rb
@@ -0,0 +1,5 @@
+class AddUserToWeightRecords < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :weight_records, :user, null: false, foreign_key: true
+  end
+end
